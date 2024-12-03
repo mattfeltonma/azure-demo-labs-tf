02afc9b243afa22791d4efd143d24ec557cb04b1
Matt Felton <matt.felton@feltonma.com>
2024-12-02 20:36:49 -0500

    modified - improved aoai module
    * Added a deployment of gpt-4o to base AOAI module

--- modules/aoai/main.tf
+++ modules/aoai/main.tf
@@ -22,16 +22,24 @@ resource "azurerm_cognitive_account" "openai" {
 ## Create a deployment for OpenAI's GPT-4o
 ##
 resource "azurerm_cognitive_account_deployment" "openai_deployment" {
-  account_id = azurerm_cognitive_account.openai.id
-  name       = "gpt-4o"
-  location   = var.location
+  depends_on = [
+    azurerm_cognitive_account.openai
+  ]
+
+  name = "gpt-4o"
+  cognitive_account_id = azurerm_cognitive_account.openai.id
+  model {
+    format = "OpenAI"
+    name = "gpt-4o"
+  }
 }
 
 ## Create diagnostic settings
 ##
 resource "azurerm_monitor_diagnostic_setting" "diag" {
-
-  depends_on = [azurerm_cognitive_account.openai]
+  depends_on = [
+    azurerm_cognitive_account_deployment.openai_deployment
+  ]
 
   name                       = "diag"
   target_resource_id         = azurerm_cognitive_account.openai.id
